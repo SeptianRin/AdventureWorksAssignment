3b011261c00eab457eb723a503fb9e28c3ada4f3
SeptianRin <33709808+SeptianRin@users.noreply.github.com>
2017-12-01 05:42:11 +0700

    Add files via upload

---
+++ vcitycard2.sql
@@ -0,0 +1,19 @@
+create view [Person].[vCityAndCard2]
+as
+select cc.CardType, a.City , count(*) as count from
+[Person].[Person] as p
+inner join
+[Person].[BusinessEntityAddress] as ba
+on p.BusinessEntityID = ba.BusinessEntityID
+inner join
+[Person].[Address] as a
+on ba.AddressID = a.AddressID
+inner join
+[Sales].[PersonCreditCard] as pcc
+on p.BusinessEntityID = pcc.BusinessEntityID
+inner join
+[Sales].[CreditCard] as cc
+on pcc.CreditCardID=cc.CreditCardID
+Group by rollup (City,CardType)
+order by City ASC
+go
---
+++ vcitycard3.sql
@@ -0,0 +1,18 @@
+create view [Person].[vCityAndCard3]
+as
+select cc.CardType, a.City , count(*) as count from
+[Person].[Person] as p
+inner join
+[Person].[BusinessEntityAddress] as ba
+on p.BusinessEntityID = ba.BusinessEntityID
+inner join
+[Person].[Address] as a
+on ba.AddressID = a.AddressID
+inner join
+[Sales].[PersonCreditCard] as pcc
+on p.BusinessEntityID = pcc.BusinessEntityID
+inner join
+[Sales].[CreditCard] as cc
+on pcc.CreditCardID=cc.CreditCardID
+Group by GROUPING sets (CardType,City)
+go
